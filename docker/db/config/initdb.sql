ff9189c9c3c45d8e6d65cd9d25b3475164853f4b
Alexssandro Oliveira <alexssandro_oliveira@atlantico.com.br>
2023-04-27 18:12:57 -0300

    First Commit

---
+++ docker/db/config/initdb.sql
@@ -0,0 +1,28 @@
+-- General Tests 
+DO $$ begin raise notice 'Starting General Tests scripts.'; end; $$;
+CREATE DATABASE general_test;
+\c general_test 
+CREATE TABLE test (
+	id serial PRIMARY KEY,
+    timestamp_field timestamp NOT NULL,
+	name VARCHAR(50) NOT NULL
+);
+INSERT INTO test (timestamp_field, name) values (NOW(), 'Test1');
+INSERT INTO test (timestamp_field, name) values (NOW(), 'Test2');
+INSERT INTO test (timestamp_field, name) values (NOW(), 'Test3');
+
+-- Beltrano Restore
+DO $$ begin raise notice 'Starting Beltrano restore scripts.'; end; $$;
+CREATE DATABASE beltrano_oltp OWNER postgres;
+CREATE DATABASE beltrano_dw OWNER postgres;
+\c beltrano_oltp
+\i /etc/postgresql/beltrano_oltp_10k_pedidos.backup
+
+\c beltrano_dw
+\i /etc/postgresql/beltrano_dw.backup
+
+-- superset database
+DO $$ begin raise notice 'Starting Superset init scripts.'; end; $$;
+create DATABASE superset OWNER postgres;
+create user superset with encrypted password 'superset';
+grant all privileges on database superset to superset;
