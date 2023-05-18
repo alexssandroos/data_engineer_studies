c01743725cabbe65e8067e73ea2650f78aef78d9
Alexssandro Oliveira <alexssandro_oliveira@atlantico.com.br>
2023-05-18 20:20:48 -0300

    add raw_calendar model

---
+++ infra/dbt/beltrano/models/bronze/raw_calendar.sql
@@ -0,0 +1,45 @@
+with 
+    month_names as (
+        select 
+            aux_number
+            ,month_name
+            ,month_name_short
+        from {{ref('seed_date_aux2')}}
+    ),
+    week_names as (
+        select distinct
+            aux_number
+            ,week_name
+            ,week_name_short
+        from {{ref('seed_date_aux2')}}
+    ),
+    date_spine as (
+        {{  dbt_utils.date_spine(    
+                datepart="day",
+                start_date="cast('2000-01-01' as date)",
+                end_date="cast('2030-01-01' as date)"
+            )
+        }}
+    ),
+    dates as (
+        select
+            CAST(DATE_DAY AS DATE) AS DATE_KEY
+        from date_spine
+    ),
+    final as (
+        SELECT
+            DATE_KEY
+            ,date_part('month', date_key) as month_number
+            ,date_part('year', date_key) as year_number
+            ,date_part('day', date_key) as day_number
+            ,date_part('dow', date_key)+1  as dow 
+            ,month_names.month_name 
+            ,month_names.month_name_short
+            ,week_names.week_name
+            ,week_names.week_name_short
+        from dates
+        left join month_names on date_part('month', date_key) = month_names.aux_number
+        left join week_names on date_part('dow', date_key)+1 = week_names.aux_number
+    )
+
+select * from final
